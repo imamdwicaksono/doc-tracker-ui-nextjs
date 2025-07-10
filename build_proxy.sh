782596865b8b021f9c6777856724f6a157792bf4
Boenzoel <bonjolman@gmail.com>
2025-07-10 21:49:00 +0700

    build proxy

--- build_proxy.sh
+++ build_proxy.sh
@@ -27,25 +27,25 @@ do
     fi
 done
 
-echo "📦 Packaging builds into zip files..."
-for PLATFORM in "${PLATFORMS[@]}"
-do
-    GOOS=${PLATFORM%/*}
-    GOARCH=${PLATFORM#*/}   
-    OUTPUT_NAME=$APP_NAME"-"$GOOS"-"$GOARCH
-    if [ "$GOOS" = "windows" ]; then
-        OUTPUT_NAME+=".exe"
-    fi
-    ZIP_NAME=$OUTPUT_NAME".zip"
+# echo "📦 Packaging builds into zip files..."
+# for PLATFORM in "${PLATFORMS[@]}"
+# do
+#     GOOS=${PLATFORM%/*}
+#     GOARCH=${PLATFORM#*/}   
+#     OUTPUT_NAME=$APP_NAME"-"$GOOS"-"$GOARCH
+#     if [ "$GOOS" = "windows" ]; then
+#         OUTPUT_NAME+=".exe"
+#     fi
+#     ZIP_NAME=$OUTPUT_NAME".zip"
 
-    echo "📦 Packaging build into zip file: $ZIP_NAME"
-    # Copy .env file next to the binary for packaging if it exists
-    if [ -f .env.proxy ]; then
-        zip -j "proxy-builds/$ZIP_NAME" "proxy-builds/$OUTPUT_NAME" ".env.proxy"
-    else
-        zip -j "proxy-builds/$ZIP_NAME" "proxy-builds/$OUTPUT_NAME"
-    fi
-done
+#     echo "📦 Packaging build into zip file: $ZIP_NAME"
+#     # Copy .env file next to the binary for packaging if it exists
+#     if [ -f .env.proxy ]; then
+#         zip -j "proxy-builds/$ZIP_NAME" "proxy-builds/$OUTPUT_NAME" ".env.proxy"
+#     else
+#         zip -j "proxy-builds/$ZIP_NAME" "proxy-builds/$OUTPUT_NAME"
+#     fi
+# done
 
 
 
